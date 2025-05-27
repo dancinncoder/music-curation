660882134362e40d11d5962c7e86097b666b0837
Hamin Lee <whereanna7@gmail.com>
2025-05-27 23:45:48 +0900

    [FIX] fix build error

--- src/app/(with-header)/dashboard/page.tsx
+++ src/app/(with-header)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function DashboardPage() {
-  console.log("[getServerSession 호출]", new Error().stack);
+  // console.log("[getServerSession 호출]", new Error().stack);
 
   return <div>dashboard page</div>;
 }
